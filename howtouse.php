e67a5dcb39244335576b396c41720992d46726f0
gsgreen <32975076+gsgreen@users.noreply.github.com>
2017-11-19 21:13:33 +0700

    Add files via upload

---
+++ howtouse.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--meta-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+    
+    <!--shortcut-->
+    <link rel="shortcut icon" href="images/icon.png" title="Favicon"/>
+    
+    <!--style-->
+    <link rel="stylesheet" type="text/css" href="css/admin7-1.css">
+    <link rel="stylesheet" type="text/css" href="css/admin7-2.css">
+
+    <!--font-->
+    <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro|Work+Sans" rel="stylesheet">
+
+    <title>Classroom Schedule</title>
+  </head>
+  <body >
+    <header>
+        <img src="images/FIBO_logo.jpg" width="55" height="62" style="margin-top: 10px; margin-left: 10px">
+        <div style="font-size: 1.8em; margin-top: -50px; margin-left: 80px">
+        <b>Classroom Schedule</b>
+        </div><br>
+        </header>
+        <div style="margin-top: -30px; margin-left: 500px">
+            <a href="admin1.php" class="btn1"><b>Home</b></a>
+            <a href="#" class="btn1"><b>User Info</b></a>
+            <a href="admin3.php" class="btn1"><b>Class Info</b></a>
+            <a href="admin4.php" class="btn1"><b>Class Management</b></a>
+            <a href="#" class="btn1"><b>Summary</b></a>
+            <a href="admin6.php" class="btn1"><b>User status</b></a>
+            <a href="admin7.php" class="btn1"><b>Complete</b></a>
+        </div>
+
+        <center>
+        <b>ADMIN</b>
+        <br>
+        <img src="howtouse/1.png" width="480" height="320"  >
+        </center>
+        <br>
+        <center>
+        <b>USER</b>
+        <br>
+        <img src="howtouse/2.png" width="480" height="320"  >
+        </center>
+        <br>
+  </body>
+</html>
